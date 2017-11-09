00889658746bdcbe03859c5312e41e1027fa4774
Daniel García Moreno <danigm@wadobo.com>
2017-11-09 20:22:17 +0100

    flatpak working with rust 1.21 freedesktop sdk extension

--- flatpak/flatpak-build.sh
+++ flatpak/flatpak-build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-flatpak-builder --repo=repo rust-sdk org.freedesktop.Sdk.Extension.rust-120.json
+flatpak-builder --repo=repo rust-sdk org.freedesktop.Sdk.Extension.rust-121.json
 flatpak-builder --repo=repo fractal org.gnome.Fractal.json
 
 #flatpak --user remote-add --no-gpg-verify --if-not-exists fractal-repo repo
-#flatpak --user install fractal-repo org.freedesktop.Sdk.Extension.rust-120
+#flatpak --user install fractal-repo org.freedesktop.Sdk.Extension.rust-121
 #flatpak --user install fractal-repo org.gnome.Fractal
