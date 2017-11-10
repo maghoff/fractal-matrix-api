a1d5be705ee95b9508af9d6fd4c99c5fb116cae7
Daniel García Moreno <danigm@wadobo.com>
2017-11-10 10:36:14 +0100

    Flatpak updated using the flathub rust-stable extension

--- flatpak/flatpak-build.sh
+++ flatpak/flatpak-build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-flatpak-builder --repo=repo rust-sdk org.freedesktop.Sdk.Extension.rust-121.json
+#flatpak --user install flathub org.freedesktop.Sdk.Extension.rust-stable
 flatpak-builder --repo=repo fractal org.gnome.Fractal.json
 
 #flatpak --user remote-add --no-gpg-verify --if-not-exists fractal-repo repo
-#flatpak --user install fractal-repo org.freedesktop.Sdk.Extension.rust-121
 #flatpak --user install fractal-repo org.gnome.Fractal
