57b374e04a5fe862d0768dbcee26ac26c6ebb880
Daniel García Moreno <danigm@wadobo.com>
2018-01-29 19:32:04 +0100

    Build script with DEBUG and RELEASE mode
    I've added the env var CARGO_HOME pointing to target directory to make
    it cacheable when you build Fractal with builder to speed up building.
    
    I've also added an env var check to difference between DEBUG mode and
    RELEASE mode so you can configure GnomeBuilder to build the debug
    version.

--- scripts/cargo.sh
+++ scripts/cargo.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-cargo build --release -p fractal-gtk && cp $1/target/release/fractal-gtk $2
+export CARGO_HOME=$1/target/cargo-home
+
+if [[ $DEBUG = true ]]
+then
+    echo "DEBUG MODE"
+    cargo build -p fractal-gtk && cp $1/target/debug/fractal-gtk $2
+else
+    echo "RELEASE MODE"
+    cargo build --release -p fractal-gtk && cp $1/target/release/fractal-gtk $2
+fi
