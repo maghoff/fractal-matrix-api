f355fdc3907841e39a344aefad29df73824863a3
Julian Sparber <julian@sparber.net>
2018-03-03 19:47:01 +0100

    add spacing to load more button and center it

--- fractal-gtk/src/app.rs
+++ fractal-gtk/src/app.rs
@@ -2480,9 +2480,17 @@ impl App {
             .get_object::<gtk::ListBox>("message_list")
             .expect("Can't find message_list in ui file.");
 
+        let row = gtk::ListBoxRow::new();
+        row.set_activatable(false);
+        row.set_selectable(false);
         let btn = self.op.lock().unwrap().load_more_btn.clone();
+        btn.set_halign(gtk::Align::Center);
+        btn.set_margin_top (12);
+        btn.set_margin_bottom (12);
         btn.show();
-        messages.add(&btn);
+        row.add(&btn);
+        row.show();
+        messages.add(&row);
 
         let op = self.op.clone();
         btn.connect_clicked(move |_| { op.lock().unwrap().load_more_messages(); });
