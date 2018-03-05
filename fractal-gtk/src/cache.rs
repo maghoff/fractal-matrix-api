48d127a0c4483638081d4eb53fdfdc51fff3856c
Eisha CHEN-YEN-SU <chenyensu0@gmail.com>
2018-03-05 18:26:29 +0100

    cache.rs: Add the last viewed messages
    This adds the last viewed messages in the cache data so that they
    are stored in the cache when Fractal is closed.
    
    Closes https://gitlab.gnome.org/danigm/fractal/issues/75

--- fractal-gtk/src/cache.rs
+++ fractal-gtk/src/cache.rs
@@ -1,5 +1,6 @@
 extern crate serde_json;
 
+use std::collections::HashMap;
 use std::fs::File;
 use std::fs::remove_dir_all;
 use std::io::prelude::*;
@@ -10,16 +11,25 @@ use error::Error;
 use fractal_api::util::cache_path;
 use globals;
 
+use types::Message;
+
 #[derive(Serialize, Deserialize)]
 pub struct CacheData {
     pub since: String,
     pub rooms: RoomList,
+    pub last_viewed_messages: HashMap<String, Message>,
     pub username: String,
     pub uid: String,
 }
 
 
-pub fn store(rooms: &RoomList, since: String, username: String, uid: String) -> Result<(), Error> {
+pub fn store(
+    rooms: &RoomList,
+    last_viewed_messages: HashMap<String, Message>,
+    since: String,
+    username: String,
+    uid: String
+) -> Result<(), Error> {
     let fname = cache_path("rooms.json")?;
 
     let mut cacherooms = rooms.clone();
@@ -34,6 +44,7 @@ pub fn store(rooms: &RoomList, since: String, username: String, uid: String) -> 
     let data = CacheData {
         since: since,
         rooms: cacherooms,
+        last_viewed_messages: last_viewed_messages,
         username: username,
         uid: uid,
     };
