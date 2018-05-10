0269ede02732bc409ae14a449885d2b0666c2b32
Quentin Gliech <quentingliech@gmail.com>
2018-05-10 16:55:49 +0200

    scripts/cargo.sh: Specify the Cargo.toml location

--- scripts/cargo.sh
+++ scripts/cargo.sh
@@ -5,8 +5,8 @@ export CARGO_HOME=$1/target/cargo-home
 if [[ $DEBUG = true ]]
 then
     echo "DEBUG MODE"
-    cargo build -p fractal-gtk && cp $1/target/debug/fractal-gtk $2
+    cargo build --manifest-path $1/Cargo.toml -p fractal-gtk && cp $1/target/debug/fractal-gtk $2
 else
     echo "RELEASE MODE"
-    cargo build --release -p fractal-gtk && cp $1/target/release/fractal-gtk $2
+    cargo build --manifest-path $1/Cargo.toml --release -p fractal-gtk && cp $1/target/release/fractal-gtk $2
 fi
