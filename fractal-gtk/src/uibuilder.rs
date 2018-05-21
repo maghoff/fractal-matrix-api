47c79baafe2557428531603a7c15663b06232280
Eisha CHEN-YEN-SU <chenyensu0@gmail.com>
2018-05-21 18:18:40 +0200

    UI: Implement the new room chooser design
    Closes https://gitlab.gnome.org/World/fractal/issues/221

--- fractal-gtk/src/uibuilder.rs
+++ fractal-gtk/src/uibuilder.rs
@@ -28,6 +28,8 @@ impl UI {
                .expect("Can't load ui file: members.ui");
         builder.add_from_resource("/org/gnome/Fractal/ui/markdown_popover.ui")
                .expect("Can't load ui file: markdown_popover.ui");
+        builder.add_from_resource("/org/gnome/Fractal/ui/server_chooser_menu.ui")
+               .expect("Can't load ui file: server_chooser_menu.ui");
         builder.add_from_resource("/org/gnome/Fractal/ui/stickers_popover.ui")
                .expect("Can't load ui file: stickers_popover.ui");
 
