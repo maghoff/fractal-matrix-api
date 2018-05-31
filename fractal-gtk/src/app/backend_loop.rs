bb8bdda6a43d507af1276156b45beb799724ba9f
Eisha CHEN-YEN-SU <chenyensu0@gmail.com>
2018-05-31 20:03:59 +0200

    backend_loop.rs: Remove `FinishDirectorySearch`
    This removes this response as it is not used by the backend anymore.
    
    See https://gitlab.gnome.org/World/fractal/issues/221

--- fractal-gtk/src/app/backend_loop.rs
+++ fractal-gtk/src/app/backend_loop.rs
@@ -95,9 +95,6 @@ pub fn backend_loop(rx: Receiver<BKResponse>) {
                 Ok(BKResponse::DirectorySearch(rooms)) => {
                     APPOP!(set_directory_rooms, (rooms));
                 }
-                Ok(BKResponse::FinishDirectorySearch) => {
-                    APPOP!(finish_directory_search);
-                }
 
                 Ok(BKResponse::JoinRoom) => {
                     APPOP!(reload_rooms);
